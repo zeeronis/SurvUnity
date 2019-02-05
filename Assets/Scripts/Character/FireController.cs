962002651c717c7e4e6cf83e281dad8a5b26f56d
void <zeeronis@gmail.com>
2019-02-05 17:47:56 +0300

    Add shaders: Lava, ghost Add ghost enemy prefab

---
+++ Assets/Scripts/Character/FireController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Survive
+{
+    public class FireController : MonoBehaviour
+    {
+        [SerializeField]
+        private Bullet _bulletPrefab;
+
+        [SerializeField]
+        private Transform _bulletStartPosition;
+
+        [SerializeField]
+        private float _fireDelay;
+
+        [SerializeField]
+        private string _targetLayer;
+
+        private Character _enemy;
+        public Character Enemy => _enemy;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            TriggeHandle(other);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            TriggeHandle(other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer(_targetLayer))
+            {
+                var enemy = other.GetComponent<Character>();
+
+                if (enemy != null && _enemy == enemy)
+                {
+                    _enemy = null;
+                    StopCoroutine("CoFire");
+                }
+            }
+        }
+
+
+        private void TriggeHandle(Collider other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer(_targetLayer) && _enemy == null)
+            {
+                var enemy = other.GetComponent<Character>();
+
+                if (enemy != null)
+                {
+                    _enemy = enemy;
+                    StopAllCoroutines();
+                    StartCoroutine("CoFire");                                                               //Improvement
+                }
+            }
+        }
+
+        private IEnumerator CoFire()
+        {
+            while (true)
+            {
+                if (_enemy == null)
+                {
+                    break;
+                }
+
+                Vector3 direction = (_enemy.transform.position - _bulletStartPosition.position).normalized;
+                direction.y = 0;
+                var bullet = Instantiate(_bulletPrefab, _bulletStartPosition.position, Quaternion.identity);
+                bullet.Init(direction);
+
+                yield return new WaitForSeconds(_fireDelay);
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Shaders/ScrollingUVs.shader
@@ -0,0 +1,45 @@
+﻿Shader "TextureAnimation/ScrollingUVs"
+{
+    Properties
+    {
+        _MainTint ("Diffuse Tint", Color) = (1,1,1,1)
+        _MainTex ("Base (RGB)", 2D) = "white" {}
+        _ScrollXSpeed("X Scroll Speed", Range(0,10)) = 2
+        _ScrollYSpeed("Y Scroll Speed", Range(0,10)) = 2
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        #pragma surface surf Lambert
+        
+        fixed4 _MainTint;
+        fixed _ScrollXSpeed;
+        fixed _ScrollYSpeed;
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        void surf (Input IN, inout SurfaceOutput o)
+        {
+           fixed2 scrolledUV = IN.uv_MainTex;
+
+           fixed xScrollValue = _ScrollXSpeed * _Time;
+           fixed yScrollValue = _ScrollYSpeed * _Time;
+
+           scrolledUV += fixed2(xScrollValue, yScrollValue);
+
+           half4 c = tex2D(_MainTex, scrolledUV);
+           o.Albedo = c.rbg * _MainTint;
+           o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
